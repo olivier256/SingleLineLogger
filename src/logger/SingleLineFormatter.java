e00f169a627772745b0b0e73ea20fa5dffa6297e
olivier256 <60171288+olivier256@users.noreply.github.com>
2024-11-22 15:41:12 +0100

    Add files via upload
    Don't output "null:" if loggerName is null.

--- src/logger/SingleLineFormatter.java
+++ src/logger/SingleLineFormatter.java
@@ -11,23 +11,40 @@ import java.util.logging.SimpleFormatter;
 /** @see SimpleFormatter */
 public class SingleLineFormatter extends Formatter {
 
-	private static final String FORMAT = "%1$tb %1$td, %1$tY %1$tk:%1$tM:%1$tS %2$s %3$s: %4$s";
+	private static final String FIRST_PARAMETER_AS_DATE_TIME = "%1$tb %1$td, %1$tY %1$tk:%1$tM:%1$tS";
+	private static final String FORMAT = FIRST_PARAMETER_AS_DATE_TIME + " %2$s %3$s: %4$s";
+	private static final String FORMAT_WITHOUT_LOGGER = FIRST_PARAMETER_AS_DATE_TIME + " %2$s: %3$s";
 
 	@Override
 	public String format(LogRecord logRecord) {
 		long millis = logRecord.getMillis();
 		LocalDateTime ldt = LocalDateTime.ofEpochSecond(millis / 1000, 0, ZoneOffset.ofHours(+1));
+		String source = buildSource(logRecord);
+		String loggerName = logRecord.getLoggerName();
+		String message = formatMessage(logRecord);
+		String throwable = buildThrowable(logRecord);
+		if (loggerName == null) {
+			return String.format(FORMAT_WITHOUT_LOGGER, ldt, source, message, throwable);
+		} else {
+			return String.format(FORMAT, ldt, source, loggerName, message, throwable);
+		}
+	}
+
+	private String buildSource(LogRecord logRecord) {
 		String source;
 		if (logRecord.getSourceClassName() != null) {
 			source = logRecord.getSourceClassName();
 			if (logRecord.getSourceMethodName() != null) {
-				source += " " + logRecord.getSourceMethodName();
+				source += "." + logRecord.getSourceMethodName() + "()";
 			}
 		} else {
 			source = logRecord.getLoggerName();
 		}
-		String message = formatMessage(logRecord);
-		String throwable = "";
+		return source;
+	}
+
+	private String buildThrowable(LogRecord logRecord) {
+		String throwable = null;
 		if (logRecord.getThrown() != null) {
 			StringWriter sw = new StringWriter();
 			PrintWriter pw = new PrintWriter(sw);
@@ -36,7 +53,7 @@ public class SingleLineFormatter extends Formatter {
 			pw.close();
 			throwable = sw.toString();
 		}
-		return String.format(FORMAT, ldt, source, logRecord.getLoggerName(), message, throwable);
+		return throwable;
 	}
 
 }
